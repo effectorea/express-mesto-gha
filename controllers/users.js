552dd2404acfe33ed1aa56008823d7b0d7430e59
Romanov Eugene <effectorea@yandex.ru>
2022-04-12 17:54:58 +0300

    creating user functionality

--- controllers/users.js
+++ controllers/users.js
@@ -2,24 +2,55 @@ const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
+    .then((users) => res.status(200).send({ data: users }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
-
-/* module.exports.getUserById = (req, res) => {
-  User.findById(req.params.id)
-  .then((user) => {
-    if (!user) {
-      res.send({ message: `Такого пользователя c id${req.params.id}  не существует`});
-      return;
-  }
-  res.send(user);
-}) */
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.status(200).send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.getUserById = (req, res) => {
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({
+          message: `Такого пользователя c id${req.params.userId}  не существует`,
+        });
+      }
+      return res.status(200).send(user);
+    })
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.updateProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({
+          message: `Такого пользователя c id${req.user._id}  не существует`,
+        });
+      }
+      return res.status(200).send(user);
+    })
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({
+          message: `Такого пользователя c id${req.user._id}  не существует`,
+        });
+      }
+      return res.status(200).send(user);
+    })
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
-const { getUsers, createUser } = require('../controllers/users');
+const {
+  getUsers, createUser, getUserById, updateProfile, updateAvatar,
+} = require('../controllers/users');
 
 router.get('/', getUsers);
 router.post('/', createUser);
+router.get('/:userId', getUserById);
+router.patch('/me', updateProfile);
+router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
